f15f289e107eac11be15513340800dc288d87d76
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-16 16:28:25 +0200

    Exercice 4

--- src/net/lecnam/ecue2/tp3/Rectangle.java
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -2,16 +2,24 @@ package net.lecnam.ecue2.tp3;
 
 public class Rectangle {
 
-    double longeur;
+    double longueur;
     double largeur;
     Point origine ;
 
-    public Rectangle(Point origine,double longeur, double largeur){
+    public Rectangle(Point origine,double longueur, double largeur){
         this.origine = origine;
-        this.longeur = longeur;
+        this.longueur = longueur;
         this.largeur = largeur;
     }
-    public Rectangle(double x, double y, double longeur, double largeur){
-        this(new Point(x,y), longeur , largeur);
+    public Rectangle(double x, double y, double longueur, double largeur){
+        this(new Point(x,y), longueur , largeur);
+    }
+
+    public double retourneSurface() {
+        return this.longueur * this.largeur ;
+    }
+
+    public void translate(double dx, double dy){
+        this.origine.translate(dx,dy);
     }
 }
