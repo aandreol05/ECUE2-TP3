dc7e9d1953597725bccec63b74c7d001e48b7d3e
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-16 15:58:30 +0200

    Exercice 1

--- src/net/lecnam/ecue2/tp3/Exec.java
+++ src/net/lecnam/ecue2/tp3/Exec.java
@@ -7,6 +7,14 @@ public class Exec  {
 
     public static void main(String[] args) {
 
-    }
+        Point p1 = new Point(3,7) ;
+        Point p2 = new Point(5,4) ;
+        System.out.println(p1.equals(p2));
 
+        System.out.println(p1.toString());
+
+        double distance = p1.retourneDistance(p2);
+        System.out.println(distance);
+
+    }
 }
---
+++ src/net/lecnam/ecue2/tp3/Point.java
@@ -0,0 +1,31 @@
+package net.lecnam.ecue2.tp3;
+
+public class Point {
+    double x;
+    double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    };
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point point = (Point) obj ;
+        return Double.compare(point.x, x ) == 0 && Double.compare(point.y, y ) == 0 ;
+    }
+
+    @Override
+    public String toString() {
+        return "Point {x=" + x + ",y=" + y + "}";
+    }
+
+    public double retourneDistance(Point p){
+        double x2MoinX1 = p.x - x;
+        double y2MoinY1 = p.y - y;
+
+        return Math.sqrt(x2MoinX1 * x2MoinX1 + y2MoinY1 * y2MoinY1);
+    }
+}
