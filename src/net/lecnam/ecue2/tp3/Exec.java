7084c9b3e091d2991e194b60b37bd572b78cb7ea
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-16 16:03:56 +0200

    Exercice 2

--- src/net/lecnam/ecue2/tp3/Exec.java
+++ src/net/lecnam/ecue2/tp3/Exec.java
@@ -7,6 +7,7 @@ public class Exec  {
 
     public static void main(String[] args) {
 
+        //exercice 1
         Point p1 = new Point(3,7) ;
         Point p2 = new Point(5,4) ;
         System.out.println(p1.equals(p2));
@@ -15,6 +16,9 @@ public class Exec  {
 
         double distance = p1.retourneDistance(p2);
         System.out.println(distance);
+
+        //exercice 2
+
 
     }
 }
---
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -0,0 +1,9 @@
+package net.lecnam.ecue2.tp3;
+
+public class Rectangle {
+
+    double longeur;
+    double largeur;
+    Point origine = new Point(2,8);
+
+}
