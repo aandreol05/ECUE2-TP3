0ff6a353f9c09bb0c4becd1cc0b534e572a1ec4d
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-21 14:07:06 +0200

    Exercice 14

--- src/net/lecnam/ecue2/tp3/Rectangle.java
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -39,4 +39,21 @@ public class Rectangle {
                 Double.compare(rectangle.largeur, largeur) == 0 &&
                 rectangle.origine.equals(this.origine));
     }
+
+    public boolean contient(Carre c) {
+        if (c == null) return false;
+
+        double xMinRect = this.origine.x;
+        double xMaxRect = this.origine.x + this.longueur;
+        double yMinRect = this.origine.y;
+        double yMaxRect = this.origine.y + this.largeur;
+
+        double xMinCarre = c.origine.x;
+        double xMaxCarre = c.origine.x + c.cote;
+        double yMinCarre = c.origine.y;
+        double yMaxCarre = c.origine.y + c.cote;
+
+        return (xMinCarre >= xMinRect && xMaxCarre <= xMaxRect) &&
+                (yMinCarre >= yMinRect && yMaxCarre <= yMaxRect);
+    }
 }
